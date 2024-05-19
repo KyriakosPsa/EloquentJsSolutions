d47180f28c190b5963e797a011006a7ab6c18914
Kyriakos Psallidas <kyriakos.psallidas@gmail.com>
2024-05-19 19:21:39 +0300

    Chapter 5 exercises

--- 04_Data_Structures/reversingarray.js
+++ 04_Data_Structures/reversingarray.js
@@ -1,18 +1,17 @@
 /* he first, reverseArray, should take an array as argument 
 and produce a new array that has the same elements in the inverse order.
- The second, reverseArrayInPlace, should do what the reverse method does:
+The second, reverseArrayInPlace, should do what the reverse method does:
 modify the array given as argument by reversing its elements. 
 Neither may use the standard reverse methods*/
 
 function reverseArray(array) {
   let reversed = [];
   for (let element of array) reversed.unshift(element);
-  return reversed
+  return reversed;
 }
 
 let myArray = ["A", "B", "C"];
 console.log(reverseArray(myArray));
-
 
 function reverseArrayInPlace(array) {
   for (let i = 0; i < Math.floor(array.length / 2); i++) {
